99da8a04bcb22722020bbcf301abd50cf5bc02b7
alibas <basitali@humanfocus.co.uk>
2017-05-04 14:21:14 +0500

    Merge branch 'master' of https://github.com/basitali89hfi/FirstTestApp
    # Conflicts:
    #	app/src/main/java/com/valdesekamdem/library/mdtoastsample/MainActivity.java

--- app/src/main/java/com/valdesekamdem/library/mdtoastsample/MainActivity.java
+++ app/src/main/java/com/valdesekamdem/library/mdtoastsample/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // test changesvcvbvbc zxcvxcvxcvxcvxcvxcvxcvc
+    //dfddf
     }
 
     public void showInfoToast(View view) {
