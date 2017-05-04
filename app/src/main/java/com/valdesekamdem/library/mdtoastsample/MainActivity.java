ef20399d7d0d11b485b49d554b9ba177f5c5a805
alibas <basitali@humanfocus.co.uk>
2017-05-04 11:47:47 +0500

    Testing

--- app/src/main/java/com/valdesekamdem/library/mdtoastsample/MainActivity.java
+++ app/src/main/java/com/valdesekamdem/library/mdtoastsample/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // test changesvcvbvbc zxcvxcvxcvxcvxcvxcvxcvc
+        // test changesvcvbvbc zxcvxcvxcvxcvxcvxcvxcvc
 
     }
 
